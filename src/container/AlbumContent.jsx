b37ba4db1e4f0f8b86c8eee462be653a90057076
OTecHeun <tjdgms1145a@naver.com>
2022-12-08 20:56:12 +0900

    '입니다'

--- src/container/AlbumContent.jsx
+++ src/container/AlbumContent.jsx
@@ -5,7 +5,7 @@ function AlbumContent(props) {
     return (
         <div className='arlimContainer'>
             <div className='arlimHomeTitle'>
-                <div className='menuTitle'>앨범입니다.</div>
+                <div className='menuTitle'>앨범 입니다.</div>
                 <div className="plusBtn">
                     <i className="fas fa-plus"></i>
                 </div>
